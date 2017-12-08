50d140189d233bb4662c9da131c295da88978fd1
Sargis Yonan <sargisy@gmail.com>
2017-12-08 20:51:50 +0000

    camera_service is looking ok, still working out some of the error checking:wq

--- cameras/camera_service.py
+++ cameras/camera_service.py
@@ -6,28 +6,90 @@ import os
 
 cameras = [False,False]
 
-class cameraService:
-	def __init__(self):
 
+class Camera:
+	def __init__(self, cam_id,name):
+		self.cam_id = int(cam_id)
+		self.name = name 
+
+	def startVideo(self, timeout=10):
+		"""attempt to open video capture"""
+		startTime = time.clock()
+		while True:	
+			self.video_capture = cv2.VideoCapture(self.cam_id)
+			if self.video_capture.isOpened():
+				print("opened stream for camera"+str(self.cam_id))
+				break
+			else:
+				if startTime + timeout > time.clock():
+					raise(Exception("Could not initialize camera"+str(self.cam_id)))
+
+
+class cameraService:
+	""" a module that maintains the cameras (reboots them if junk fails)"""
+	def __init__(self,retry = False):
 		#check with os about available devices:
-		avail_cameras = []
+		self.avail_cameras = []
 		for dev in os.listdir("/dev/"):
 			if dev[0:5] == "video":
-				avail_cameras.append(int(dev[5]))
-		if not len(avail_cameras) == 2:
+				self.avail_cameras.append(int(dev[5]))
+		if not len(self.avail_cameras) == 2:
 			raise(Exception(
-			"Error! Less than two cameras detected:"+str(avail_cameras) ))
+			"Error! Less than two cameras detected:"+str(self.avail_cameras) ))
+		print("found cameras", self.avail_cameras)
+		self.cam_dict = {"front":None, "down":None}
+
+		#now start up these cameras!	
+		self.initCamera("front", retry)
+		self.initCamera("down", retry)
+
+	def initCamera(self, cam_name, retry = False):
+		if cam_name == "front":
+			cam_id = self.avail_cameras[0]
+		if cam_name == "down":
+			cam_id = self.avail_cameras[1]
+		while True:
+			cam	= Camera(cam_id, cam_name)
+			try:
+				cam.startVideo()
+			except Exception as e:
+				if retry:
+					continue
+				raise(e)
+						
+			self.cam_dict[cam_name] = cam 
+			print("started video capture for camera",cam_name)
+
+	def snap(self, camname):
+		"""attempt a snapshot, enter "front" or "down" to indicate which camera you like"""
+		cam = self.cam_dict[cam_name]
+
+		"""attempt a snapshot, if we fail we'll restart the camera"""
+		#first, are we open?
+		if not cam.video_capture.isOpened():
+			raise(Exception("can't snap unless camera is open!"))
+		retval, img = cam.video_capture.read()
+		if retval:
+			#first, make sure that the image exists!
+			if any( [dim == 0 for dim in img.shape] ):
+				raise(Exception("snapped an empty image, you should restart the camera"))
+			else:
+				return img
+		else:
+			return False
 
 if __name__=="__main__":
-		cs = cameraService()		
+		cs = cameraService(retry = True)		
+		img = cs.snap("front")
+		if img:
+			cv2.write("front_img.png")
+			print("snapped a new front image, check it out!")
+		else:
+			print("failed to snap a front image :(")
 
 
-
-#def openCamera(camera):
-#	id = camera.get(
-
+#-------------------------- REFERENCE JUNK BELOW -------------------------------
 quit()
-
 while True:
 		for i in [-1,-2]:
 			cameras[i] = cv2.VideoCapture(i)
